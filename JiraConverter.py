6c95305262cc2892a75f2ac5d485dcc2ea5f202f
Moises Baltazar <me@moisesb.com>
2022-10-07 12:03:42 -0400

    Add initial implementation of Manual CLI tools for JIRA

---
+++ JiraConverter.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+"""
+This CLI utility converts a CSV file from Petra's error format into a format
+that can be imported into Jira, using companion tool `JiraCsvImporter`
+"""
+
+import pandas as pd
+import click
+
+
+# Lookup Maps
+MAP_SITE_TO_PROJECT_ACRONYM = {
+    1: "AABCMGH",
+    2: "AABCUCLA",
+    3: "AABCUMN",
+    4: "AABCWU",
+    # (default value) NA values will be filled with 0, before replacement:
+    0: "AABCWU",
+}
+MAP_COLOR_TO_MIN_DAYS = dict(
+    RED=7,
+    ORANGE=18,
+    YELLOW=28,
+    GREEN=35,
+    # Anything not listed above will be treated as "DEFAULT", including NA values
+    DEFAULT=0,
+)
+
+LIST_JIRA_FIELDS_IN_ORDER = [
+    "project",
+    "summary",
+    "description",
+    "error_code",
+    "event_n",
+    "event_date",
+    "component",
+]
+
+
+def ticket_old_enough(df_row: pd.Series) -> bool:
+    """Return True if the ticket row is old enough to be created in Jira"""
+    color = df_row.code if df_row.code in MAP_COLOR_TO_MIN_DAYS else "DEFAULT"
+    min_days = MAP_COLOR_TO_MIN_DAYS[color]
+    return df_row.issue_age >= min_days
+
+
+def read_csv_cols(csv_path: str, cols: set) -> pd.DataFrame:
+    """Read the CSV file into a DataFrame and make sure expected columns exist
+
+    Args:
+        csv_path: Path to the CSV file
+        cols: Set of column names to check for (all other columns will be dropped)
+
+    Returns:
+        DataFrame with only the columns in `cols`
+    """
+    df = pd.read_csv(csv_path)
+    actual = set(df.columns)
+
+    if not actual.issuperset(cols):
+        raise ValueError(f"File: {csv_path} is missing columns: {cols - actual}")
+
+    return df[cols].copy()
+
+
+@click.command("cli", context_settings={"show_default": True}, help=__doc__)
+@click.argument("csv_file", type=click.Path(exists=True))
+@click.option(
+    "--output",
+    "-o",
+    type=click.Path(writable=True),
+    default="./jira_tickets.csv",
+    help="CSV file to write Jira Tickets to",
+)
+@click.option(
+    "--show-dropped", "-v", is_flag=True, help="Show dropped tickets (`dropped.csv`)"
+)
+def to_jira_df(
+    csv_file: str,
+    output: str = "./jira_tickets.csv",
+    show_dropped: bool = False,
+):
+    df = read_csv_cols(
+        csv_file,
+        {
+            "site",
+            "code",
+            "issue_age",
+            "subject",
+            "issueCode",
+            "redcap_event",
+            "datatype",
+            "reason",
+            "study_id",
+        },
+    )
+
+    # Give Jira specific names
+    df = df.rename(
+        columns={
+            "issueCode": "error_code",
+            "redcap_event": "event_n",
+            "datatype": "component",
+        }
+    )
+
+    df["project"] = df.site.fillna(0).astype(int).replace(MAP_SITE_TO_PROJECT_ACRONYM)
+    df["summary"] = df["subject"] + "." + df["event_n"] + " - " + df["component"]
+    df["description"] = (
+        df["reason"]
+        + "\nevent_date: "
+        + df["event_date"]
+        + "\nstudy_id: "
+        + df["study_id"]
+    )
+
+    # Convert to int, before => after: `21 days` => `21`, `0 days` => `0`, `nan` => `0`
+    df.issue_age = (
+        df.issue_age.str.extract(r"(\d+) days", expand=False).fillna(0).astype(int)
+    )
+
+    is_old_enough = df.apply(ticket_old_enough, axis=1)
+
+    # These tickets are too young/soon to create Jira tickets for
+    dropped_tickets = df[~is_old_enough].copy()
+    if show_dropped:
+        dropped_tickets.to_csv("dropped.csv", index=False)
+
+    # These tickets are old enough
+    to_jira = df[is_old_enough].copy()
+
+    # Re-order the fields
+    to_jira = to_jira[LIST_JIRA_FIELDS_IN_ORDER]
+    to_jira.to_csv(output, index=False)
+
+
+if __name__ == "__main__":
+    to_jira_df()
---
+++ JiraCsvImporter.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python3
+"""
+This CLI utility import CSV files into Jira tickets.
+"""
+
+import click
+import pandas as pd
+from ccf.config import LoadSettings
+
+from creds import *
+from jira import JIRA, Issue
+from JiraConverter import LIST_JIRA_FIELDS_IN_ORDER, read_csv_cols
+
+config = LoadSettings()
+secret = pd.read_csv(config["config_files"]["secrets"])
+
+
+LIST_VALID_COMPONENTS = [
+    "Actigraphy",
+    "ASA24",
+    "PsychoPy",
+    "RAVLT",
+    "REDCap",
+    "TLBX",
+]
+
+# Custom Fields for Jira
+MAP_FIELD_TO_N = {
+    "error_code": 10800,
+    "event_n": 10801,
+    "event_date": 10802,
+}
+
+
+def as_search_field(field):
+    if field not in MAP_FIELD_TO_N:
+        raise ValueError(f"Invalid field: {field}")
+    field_n = MAP_FIELD_TO_N[field]
+    return f"cf[{field_n}]"
+
+
+def as_field(field):
+    if field not in MAP_FIELD_TO_N:
+        raise ValueError(f"Invalid field: {field}")
+    field_n = MAP_FIELD_TO_N[field]
+    return f"customfield_{field_n}"
+
+
+def create_jira_ticket(row: pd.Series) -> Issue:
+    """Create a Jira ticket from a row in the DataFrame
+
+    Args:
+            row: row from DataFrame
+
+    Returns:
+            Jira ticket
+
+    Side Effects:
+            Creates a Jira ticket on the server
+    """
+    issue_dict = {
+        "project": row.project,
+        # "project": "AABCBLANK",
+        "summary": row.summary,
+        "description": row.description,
+        "issuetype": {"name": "Task"},
+    }
+    if pd.notna(row.error_code):
+        # Must be list of strings
+        issue_dict[as_field("error_code")] = row.error_code.split(",")
+
+    if pd.notna(row.event_n):
+        # Must be list of strings
+        issue_dict[as_field("event_n")] = row.event_n.split(",")
+
+    if pd.notna(row.event_date):
+        # TODO: Enforce format of YYYY-MM-DD
+        issue_dict[as_field("event_date")] = row.event_date
+
+    if pd.notna(row.component):
+        components = set(row.component.split(","))
+        invalid_components = components - set(LIST_VALID_COMPONENTS)
+        if len(invalid_components) > 0:
+            raise ValueError(
+                f"Invalid components: {invalid_components} for `{row.summary}`"
+            )
+
+        issue_dict["components"] = [{"name": c} for c in components]
+
+    return jira.create_issue(fields=issue_dict)
+
+
+def search_for_ticket(row: pd.Series) -> str:
+    """Search for existing Jira ticket with the same summary and error code
+
+    Args:
+        row: row from DataFrame
+
+    Returns:
+        Jira ticket key if found, else empty string
+    """
+    jql_search_query = f"summary ~ '{row.summary}'"
+
+    # Search for tickets with same Error Codes
+    if pd.notna(row.error_code):
+        error_codes = row.error_code.split(",")
+        for code in error_codes:
+            jql_search_query += f" AND {as_search_field('error_code')} = '{code}'"
+
+    issues = jira.search_issues(jql_search_query)
+    issue_keys = ",".join([i.key for i in issues])
+    return issue_keys
+
+
+def create_if_not_exists(row: pd.Series) -> str:
+    """
+    Create Jira ticket if it does not already exist. It first does a search.
+    Args:
+        row: Tickt row from DataFrame
+
+    Returns:
+        Jira ticket key
+
+    Side Effects:
+        Prints to stdout whether ticket was created or not
+    """
+    issues = search_for_ticket(row)
+    if issues:
+        print(f"Ticket(s) already exists: {issues}")
+    else:
+        if DRYRUN:
+            print(f"DRYRUN: Would create ticket: {row.summary}")
+        else:
+            result = create_jira_ticket(row)
+            issues = result.key
+            print(f"Created new ticket: {issues}")
+    return issues
+
+
+DRYRUN = False
+jira = None
+BOT_PERSONAL_ACCESS_TOKEN = (
+    secret.loc[secret.source == "JIRA_PERSONAL_ACCESS_TOKEN", "api_key"]
+    .reset_index()
+    .drop(columns="index")
+    .api_key[0]
+)
+API_URL = (
+    secret.loc[secret.source == "JIRA_API_URL", "api_key"]
+    .reset_index()
+    .drop(columns="index")
+    .api_key[0]
+)
+
+
+@click.command("cli", context_settings={"show_default": True}, help=__doc__)
+@click.argument("csv_file", type=click.Path(exists=True), default="./jira_tickets.csv")
+@click.option("-n", "--dry-run", is_flag=True, help="Do NOT create actual tickets")
+def main(csv_file, dry_run=False):
+    global DRYRUN, jira
+    DRYRUN = dry_run
+    jira = JIRA(API_URL, token_auth=BOT_PERSONAL_ACCESS_TOKEN)
+    df = read_csv_cols(csv_file, LIST_JIRA_FIELDS_IN_ORDER)
+    results = df.apply(create_if_not_exists, axis=1)
+
+    # TODO: Do something with results???
+
+
+if __name__ == "__main__":
+    main()
