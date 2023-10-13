4ef767ee6d9291d7ae1c905b7eaca8354e0c7377
petralenzini <petra.lenzini@gmail.com>
2023-10-13 13:07:48 -0500

    add crosstabs

---
+++ Issues_Summary.py
@@ -0,0 +1,33 @@
+import os
+import pandas as pd
+import datetime
+
+issuesdir="/Users/petralenzini/work/Behavioral/AABC/AABC_Behavioral_QC/AABC_Behavioral_QC/Issues/"
+issuelist=os.listdir(issuesdir)
+Allissues=pd.DataFrame()
+for i in issuelist:
+    print(i)
+    if "All_Issues" in i:
+        try:
+            iss=pd.read_csv(issuesdir+i)
+            Allissues=pd.concat([Allissues,iss],axis=0)
+        except:
+            print(i,'will not load')
+
+
+Allissues=Allissues.loc[Allissues.subject.isnull()==False]
+Allissues=Allissues.loc[Allissues.event_date.isnull()==False]
+Allissues['days']=Allissues.issue_age.str.split(expand=True)[0].astype('int')
+#filteredAll=Allissues.loc[((Allissues.code=='PINK') & (Allissues.issue_age.dt.days>7)) | ((Allissues.code=='RED') & (Allissues.issue_age.dt.days>4)) | ((Allissues.code=='RED') & (Allissues.issue_age.dt.days.isnull()==True)) |  ((Allissues.code=='ORANGE') & (Allissues.issue_age.dt.days>18)) |  ((Allissues.code=='YELLOW') & (Allissues.issue_age.dt.days>28)) |  ((Allissues.code=='GREEN') & (Allissues.issue_age.dt.days>35)) ]
+filteredAll=Allissues.loc[((Allissues.code=='PINK') & (Allissues.days>7)) | ((Allissues.code=='RED') & (Allissues.days>4)) | ((Allissues.code=='RED') & (Allissues.days.isnull()==True)) |  ((Allissues.code=='ORANGE') & (Allissues.days>18)) |  ((Allissues.code=='YELLOW') & (Allissues.days>28)) |  ((Allissues.code=='GREEN') & (Allissues.days>35)) ]
+
+
+filteredAll=filteredAll.drop_duplicates(subset=['subject', 'redcap_event','datatype'])
+filteredAll.to_csv('allissuestest.csv',index=False)
+
+print("Issues by site:",filteredAll.site.value_counts(dropna=False))
+print("Issues by datatype:",filteredAll.datatype.value_counts(dropna=False))
+print("Issues by reason:",filteredAll.reason.value_counts(dropna=False))
+filteredAll.reason.value_counts(dropna=False).to_csv('reasons539.csv',index=False)
+print("Open Issues by site")
+pd.DataFrame(filteredAll.reason.value_counts()).to_csv('testreasons2.csv')
