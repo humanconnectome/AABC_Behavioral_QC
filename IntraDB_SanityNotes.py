adc6555d94db161a453db1ba2946498696ace3d2
petralenzini <petra.lenzini@gmail.com>
2024-06-03 16:53:09 -0500

    cleanup notes

--- IntraDB_SanityNotes.py
+++ IntraDB_SanityNotes.py
@@ -14,14 +14,11 @@ intradb=pd.read_csv(config['config_files']['PCP'])
 user = intradb.user[0]
 passw = intradb.auth[0]
 HOST = "hcpi-shadow22.nrg.wustl.edu"
-projects=["AABC_UMN_ITK","AABC_UCLA_ITK","AABC_MGH_ITK"]#"AABC_WU_ITK",
+projects=["AABC_MGH_ITK","AABC_UMN_ITK","AABC_UCLA_ITK"]#"AABC_WU_ITK",
 #test
 #curlcmd0 = "curl -s -k -v -u "+user+":"+passw+" https://intradb.humanconnectome.org/data/experiments?xsiType=xnat:mrSessionData\&format=json\&columns=ID,label,project,xsiType,subject_label,URI\&project=AABC_MGH_ITK"
 #curlcmd0 = "curl -s -k -v -u "+user+":"+passw+" https://intradb.humanconnectome.org/data/experiments?xsiType=xnat:subjectData\&format=json\&columns=subject_label,yob\&project=AABC_MGH_ITK"
 
-##os.system(curlcmd0)
-#os.system(curlcmd0)
-PROJECT="AABC_WU_ITK"
 AllSanity=pd.DataFrame()
 for PROJECT in projects:
     curlcmd="curl -s -k -v -u "+user+":"+passw+" https://"+HOST+"/xapi/sanityChecksReports/project/"+PROJECT+"/failureReportCSV > "+outp+PROJECT+"_Sanity.csv"
@@ -40,14 +37,6 @@ for PROJECT in projects:
     scannotes.columns=['imageSessionLabel','scanID','scanQuality','scanNote','sessionNote']
     scannotes['scanID']=scannotes.scanID.astype(int).astype(str)
     scannotes.to_csv(outp+PROJECT+"_ScanNotes.csv")
-    #curlcmd2="curl -s -k -v -u "+user+":"+passw+" https://"+HOST+"/data/experiments?xsiType=xnat:mrSessionData\&format=json\&columns=ID,label,project,xsiType,scanner,subject_label,session_type,URI,note\&project="+PROJECT+" > "+outp+PROJECT+"_Notes.txt"
-    #os.system(curlcmd2)
-    #with open(outp+PROJECT+"_Notes.txt", 'r') as file:
-    #    data = json.load(file)
-    #notesdf = pd.json_normalize(data['ResultSet']['Result'])
-    #notesdf['sessionLabel']=notesdf.label
-    #notesdf=notesdf[['note','sessionLabel']].copy()
-    #notesdf['label']=notesdf.sessionLabel+"_sc"
 
     SanityChecksWithNotes=pd.merge(sanitydf,scannotes,how='left',on=['imageSessionLabel','scanID'])
     SanityChecksWithNotes['PROJECT']=PROJECT
@@ -56,25 +45,26 @@ for PROJECT in projects:
 
 AllSanity['imageSessionDate']=pd.to_datetime(AllSanity.imageSessionDate)
 
-#Wash U: XA30 scanner = AWP166158
-#UCLA: XA30 scanner = MRC35426
-#MGH: all scans after 10/11/23
+### These commented out filters were to drop sanity check failures due to XA30 when generating lists to sites
+### -- this issue should be resolved post-freeze, so we won't filter them out again without good reason
+### Wash U: XA30 scanner = AWP166158
+### UCLA: XA30 scanner = MRC35426
+### MGH: all scans after 10/11/23
 print(AllSanity.shape)
-sub1=AllSanity.loc[~((AllSanity.PROJECT=="AABC_MGH_ITK") & (AllSanity.imageSessionDate>"2023-10-11"))]
-print(sub1.shape)
-sub2=sub1.loc[~((sub1.scanner=="MRC35426") & (sub1.imageSessionDate>"4/20/2023"))]
-print(sub2.shape)
-sub3=sub2.loc[~((sub2.scanner=="MRC35343") & (sub2.imageSessionDate>"10/26/2023"))]
-print(sub3.shape)
-sub4=sub3.loc[~(sub3.scanner.isin(['AWP166158']))].copy()
-print(sub4.shape)
-sub5=sub4.loc[sub4.imageSessionDate<"2023-11-01"]
-print(sub5.shape)
-sub5=sub5.loc[~((sub5.imageSessionLabel.str.contains('TEST')) | (sub5.imageSessionLabel.str.contains('incomplete')) | (sub5.imageSessionLabel.str.contains('7T')) | (sub5.imageSessionLabel.str.contains('phantom')))]
+#sub1=AllSanity.loc[~((AllSanity.PROJECT=="AABC_MGH_ITK") & (AllSanity.imageSessionDate>"2023-10-11"))]
+#print(sub1.shape)
+#sub2=sub1.loc[~((sub1.scanner=="MRC35426") & (sub1.imageSessionDate>"4/20/2023"))]
+#print(sub2.shape)
+#sub3=sub2.loc[~((sub2.scanner=="MRC35343") & (sub2.imageSessionDate>"10/26/2023"))]
+#print(sub3.shape)
+#sub4=sub3.loc[~(sub3.scanner.isin(['AWP166158']))].copy()
+#print(sub4.shape)
+#sub5=sub4.loc[sub4.imageSessionDate<"2023-11-01"]
+#print(sub5.shape)
+#sub5=sub5.loc[~((sub5.imageSessionLabel.str.contains('TEST')) | (sub5.imageSessionLabel.str.contains('incomplete')) | (sub5.imageSessionLabel.str.contains('7T')) | (sub5.imageSessionLabel.str.contains('phantom')))]
 
-
-sub5.imageSessionLabel.value_counts()
-sub5.PROJECT.value_counts()
-sub5=sub5.drop(columns=['imageSessionID','label','site','ID'])
-sub5=sub5.rename(columns={'imageSessionLabel':'MR_Session'})
-sub5.to_csv(outp+'SanityChecksWithNotes_'+date.today().strftime("%d%b%Y")+'.csv',index=False)
+AllSanity.imageSessionLabel.value_counts()
+AllSanity.PROJECT.value_counts()
+AllSanity=AllSanity.drop(columns=['imageSessionID','label','site','ID'])
+AllSanity=AllSanity.rename(columns={'imageSessionLabel':'MR_Session'})
+AllSanity.to_csv(outp+'SanityChecksWithNotes_'+date.today().strftime("%d%b%Y")+'.csv',index=False)
