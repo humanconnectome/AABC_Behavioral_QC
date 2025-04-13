58e0d614f34ea4cbee8c81767ea0bf1f82d57a0a
petralenzini <petra.lenzini@gmail.com>
2025-04-13 10:30:31 -0500

    add Prestons new functions

--- functions.py
+++ functions.py
@@ -145,41 +145,88 @@ def TLBXreshape(results1):
     df2=df.loc[~(df[0]=='PIN')]
     df2.columns=cols[0]
     return df2
+#REPLACE PETRA VERSION WITH PRESTION VERSION
+#def importTLBX(siteabbrev='WU',typed='scores'):
+#    if typed=='scores':
+#        run_ssh_cmd('plenzini@login3.chpc.wustl.edu',
+#                    'find /ceph/intradb/archive/AABC_' + siteabbrev + '_ITK/resources/toolbox_endpoint_data/ -type f  -name "*Scores*" ! \( -name "*Narrow*" -o -name "*Regist*" -o -name "*catalog*" \) > /home/plenzini/tools/catTLBX/datalist.csv').stdout.read()
+#    else:
+#        run_ssh_cmd('plenzini@login3.chpc.wustl.edu',
+#                        'find /ceph/intradb/archive/AABC_'+siteabbrev+'_ITK/resources/toolbox_endpoint_data/ -type f  ! \( -name "*Scores*" -o -name "*Narrow*" -o -name "*Regist*" -o -name "*catalog*" \) > /home/plenzini/tools/catTLBX/datalist.csv').stdout.read()
+#    run_ssh_cmd('plenzini@login3.chpc.wustl.edu',
+#                'cd /home/plenzini/tools/catTLBX/cache; while read i; do cp "$i" .; done < /home/plenzini/tools/catTLBX/datalist.csv').stdout.read()
+#    #run_ssh_cmd('plenzini@login3.chpc.wustl.edu',
+#    #                    'while read i; do cp "$i" /home/plenzini/tools/catTLBX/cache/.; done < /home/plenzini/tools/catTLBX/datalist.csv').stdout.read()
+#    run_ssh_cmd('plenzini@login3.chpc.wustl.edu',
+#                'for f in /home/plenzini/tools/catTLBX/cache/*\ *; do mv "$f" "${f// /_}"; done').stdout.read()
+#    run_ssh_cmd('plenzini@login3.chpc.wustl.edu',
+#                'find /home/plenzini/tools/catTLBX/cache/ -type f > /home/plenzini/tools/catTLBX/datalist2.csv').stdout.read()
+#    run_ssh_cmd('plenzini@login3.chpc.wustl.edu',
+#                "sed -i 's/\/home\/plenzini/\/Users\/petralenzini\/chpc3/g' /home/plenzini/tools/catTLBX/datalist2.csv").stdout.read()
+#    # Using readlines()
+#    file1 = open('/Users/petralenzini/chpc3/tools/catTLBX/datalist2.csv', 'r')
+#    Lines = file1.readlines()
+#    sitedf=pd.DataFrame()
+#    count = 0
+#    # Strips the newline character
+#    for line in Lines:
+#        count += 1
+#        print(count)
+#        subsetdf=pd.read_csv(line.strip("\n"))
+#        sitedf=pd.concat([sitedf,subsetdf],axis=0)
+#        sitedf['sitestr']=siteabbrev
+#    file1.close()
+#    run_ssh_cmd('plenzini@login3.chpc.wustl.edu',
+#                "rm -f /home/plenzini/tools/catTLBX/cache/* /home/plenzini/tools/catTLBX/datalist* ").stdout.read()
+#    return sitedf
 
-#TODO move chcp details to config file
-def importTLBX(siteabbrev='WU',typed='scores'):
-    if typed=='scores':
-        run_ssh_cmd('plenzini@login3.chpc.wustl.edu',
-                    'find /ceph/intradb/archive/AABC_' + siteabbrev + '_ITK/resources/toolbox_endpoint_data/ -type f  -name "*Scores*" ! \( -name "*Narrow*" -o -name "*Regist*" -o -name "*catalog*" \) > /home/plenzini/tools/catTLBX/datalist.csv').stdout.read()
+
+def importTLBX(siteabbrev='WU',typed='scores',username='w.zijian@login3.chpc.wustl.edu'):
+    if typed=='scores': 
+        run_ssh_cmd(username,
+                    'find /ceph/intradb/archive/AABC_' + siteabbrev + '_ITK/resources/toolbox_endpoint_data/ -type f  -name "*Scores*" ! \( -name "*Narrow*" -o -name "*Regist*" -o -name "*catalog*" \) > /home/w.zijian/tools/catTLBX/datalist.csv').stdout.read()
     else:
-        run_ssh_cmd('plenzini@login3.chpc.wustl.edu',
-                        'find /ceph/intradb/archive/AABC_'+siteabbrev+'_ITK/resources/toolbox_endpoint_data/ -type f  ! \( -name "*Scores*" -o -name "*Narrow*" -o -name "*Regist*" -o -name "*catalog*" \) > /home/plenzini/tools/catTLBX/datalist.csv').stdout.read()
-    run_ssh_cmd('plenzini@login3.chpc.wustl.edu',
-                'cd /home/plenzini/tools/catTLBX/cache; while read i; do cp "$i" .; done < /home/plenzini/tools/catTLBX/datalist.csv').stdout.read()
+        run_ssh_cmd(username,
+                        'find /ceph/intradb/archive/AABC_'+siteabbrev+'_ITK/resources/toolbox_endpoint_data/ -type f  ! \( -name "*Scores*" -o -name "*Narrow*" -o -name "*Regist*" -o -name "*catalog*" \) > /home/w.zijian/tools/catTLBX/datalist.csv').stdout.read()
+    run_ssh_cmd(username,
+                'cd /home/w.zijian/tools/catTLBX/cache; while read i; do cp "$i" .; done < /home/w.zijian/tools/catTLBX/datalist.csv').stdout.read()
     #run_ssh_cmd('plenzini@login3.chpc.wustl.edu',
     #                    'while read i; do cp "$i" /home/plenzini/tools/catTLBX/cache/.; done < /home/plenzini/tools/catTLBX/datalist.csv').stdout.read()
-    run_ssh_cmd('plenzini@login3.chpc.wustl.edu',
-                'for f in /home/plenzini/tools/catTLBX/cache/*\ *; do mv "$f" "${f// /_}"; done').stdout.read()
-    run_ssh_cmd('plenzini@login3.chpc.wustl.edu',
-                'find /home/plenzini/tools/catTLBX/cache/ -type f > /home/plenzini/tools/catTLBX/datalist2.csv').stdout.read()
-    run_ssh_cmd('plenzini@login3.chpc.wustl.edu',
-                "sed -i 's/\/home\/plenzini/\/Users\/petralenzini\/chpc3/g' /home/plenzini/tools/catTLBX/datalist2.csv").stdout.read()
+    run_ssh_cmd(username,
+                'for f in /home/w.zijian/tools/catTLBX/cache/*\ *; do mv "$f" "${f// /_}"; done').stdout.read()
+    run_ssh_cmd(username,
+                'find /home/w.zijian/tools/catTLBX/cache/ -type f > /home/w.zijian/tools/catTLBX/datalist2.csv').stdout.read()
+    run_ssh_cmd(username,
+                "sed -i 's/\/home\/w.zijian/\/Users\/w.zijian\/chpc3/g' /home/w.zijian/tools/catTLBX/datalist2.csv").stdout.read()
     # Using readlines()
-    file1 = open('/Users/petralenzini/chpc3/tools/catTLBX/datalist2.csv', 'r')
+    file1 = open('/Users/w.zijian/chpc3/tools/catTLBX/datalist2.csv', 'r')
     Lines = file1.readlines()
     sitedf=pd.DataFrame()
     count = 0
     # Strips the newline character
     for line in Lines:
+        #try:
+            #count += 1
+            #subsetdf=pd.read_csv(line.strip("\n"))
+            #sitedf=pd.concat([sitedf,subsetdf],axis=0)
+            #sitedf['sitestr']=siteabbrev
+        #except Exception as e:
+            #print("Error processing file:", line.strip())
+            #print("Exception:", str(e))
         count += 1
-        print(count)
         subsetdf=pd.read_csv(line.strip("\n"))
         sitedf=pd.concat([sitedf,subsetdf],axis=0)
         sitedf['sitestr']=siteabbrev
     file1.close()
     run_ssh_cmd('plenzini@login3.chpc.wustl.edu',
                 "rm -f /home/plenzini/tools/catTLBX/cache/* /home/plenzini/tools/catTLBX/datalist* ").stdout.read()
+
+
+#    run_ssh_cmd(username,
+#                "rm -f /home/w.zijian/tools/catTLBX/cache/* /home/w.zijian/tools/catTLBX/datalist* ").stdout.read()
+
     return sitedf
+
 
 def getPCP(intradb,project,pipeline):
     # TO TO: curl this with a JSESSION ID instead:
@@ -310,6 +357,28 @@ def getredcap10Q(struct,studystr,curatedsnaps,goodies,idstring,config,restricted
     print(df.shape)
     return df, dfrestricted
 
+def getfullRedExport(studystr,auth):
+    """
+    downloads all events and fields in a redcap database
+    """
+    studydata = pd.DataFrame()
+    print(auth)
+    token=auth.loc[auth.source==studystr,'api_key'].reset_index().api_key[0]
+    print(token)
+    data = {
+        'token': token,
+        'content': 'record',
+        'format': 'json',
+        'type': 'flat',
+        'rawOrLabel': 'raw',
+        'rawOrLabelHeaders': 'raw',
+        'exportCheckboxLabel': 'false',
+        'exportSurveyFields': 'false',
+        'exportDataAccessGroups': 'false',
+        'returnFormat': 'json'
+    }
+    df = getframe(data, config['Redcap']['api_url'])
+    return df
 
 def folder_files(client, folders, extension='.csv', recursively=False):
     """
@@ -350,7 +419,6 @@ def folder_files(client, folders, extension='.csv', recursively=False):
 
 def removeIssues(dataset,issuefila,component='ASA24'):
     issues=pd.read_csv(issuefila)#'All_Issues_'+date.today().strftime("%d%b%Y")+'.csv'
-    print(issues.head())
     droplist=issues.loc[issues.datatype==component][['subject','redcap_event']].drop_duplicates()
     dset=pd.merge(dataset,droplist,on=['subject','redcap_event'],how='outer',indicator=True)
     return dset.loc[dset._merge=='left_only'].drop(columns='_merge')
@@ -358,19 +426,39 @@ def removeIssues(dataset,issuefila,component='ASA24'):
 def droprest(datain,dropvars):
     return datain.drop(columns=dropvars)
 
+###USE PRESTONS VERSION
+#def PINfirst(dataset,strname,issuefile,inventory,dropvars):
+#    dataset['subject'] = dataset.PIN.str[:10]
+#    dataset['redcap_event'] = dataset.PIN.str[11:13]
+#    print(dataset.shape)
+#    dataset2=pd.merge(dataset,inventory,on=['subject','redcap_event'],how='inner')
+#    print(issuefile)
+#    dataset3=removeIssues(dataset2,component='ASA24',issuefila=issuefile)
+#    a = list(dataset3.columns)
+#    b = dataset3[a[-3:] + a[:-3]].sort_values('PIN').drop_duplicates()
+#    print(b.shape)
+#    c = b.drop(columns=dropvars)
+#    c.loc[b.PIN != ''].to_csv("./tmp/AABC_ASA24-" + strname + "_" + date.today().strftime("%Y-%m-%d") + ".csv",index=False)
+#    return b,c
+
 def PINfirst(dataset,strname,issuefile,inventory,dropvars):
     dataset['subject'] = dataset.PIN.str[:10]
     dataset['redcap_event'] = dataset.PIN.str[11:13]
     print(dataset.shape)
     dataset2=pd.merge(dataset,inventory,on=['subject','redcap_event'],how='inner')
-    print(issuefile)
-    dataset3=removeIssues(dataset2,component='ASA24',issuefila=issuefile)
+    dataset3=removeIssues(dataset2,component='ASA24',issuefile=issuefile)
     a = list(dataset3.columns)
     b = dataset3[a[-3:] + a[:-3]].sort_values('PIN').drop_duplicates()
     print(b.shape)
+    #for the restricted folder
+    b.loc[b.PIN != ''].to_csv("./tmp/AABC_ASA24-"+ strname +"_Restricted_" + date.today().strftime("%Y-%m-%d") + '.csv', index=False)
+    #for the regular folder
     c = b.drop(columns=dropvars)
     c.loc[b.PIN != ''].to_csv("./tmp/AABC_ASA24-" + strname + "_" + date.today().strftime("%Y-%m-%d") + ".csv",index=False)
     return b,c
+
+
+
 
 #subsetfreeze
 def subsetfreeze(df,strname,listfreeze,byPIN=True,bySUB=False):
@@ -380,6 +468,8 @@ def subsetfreeze(df,strname,listfreeze,byPIN=True,bySUB=False):
         dff=df.loc[df.subject.isin(listfreeze)]
     dff.loc[dff.PIN != ''].to_csv("./tmp/Freeze1_AABC_ASA24-" + strname + "_" + date.today().strftime("%Y-%m-%d") + ".csv",index=False)
     return dff
+
+
 
 def getASA(client,folderqueue):
     TNS = pd.DataFrame(); INS = pd.DataFrame(); TS = pd.DataFrame(); Totals = pd.DataFrame(); Items = pd.DataFrame(); Resp = pd.DataFrame()
@@ -513,7 +603,190 @@ def getASA(client,folderqueue):
 
     return ALLSUBS, BIGGESTTotals,BIGGESTItems,BIGGESTResp,BIGGESTTS,BIGGESTTNS,BIGGESTINS
 
-def swapmeds(oldDF=HCPDparentmeds, medmap=MAP, medvarlist=medlist):
+#PRESTONs ASA_3
+def getASA_3(client, folderqueue):
+    start_time = time.time()
+    allsubdb_list = []
+    big_data = {'Totals': [], 'Items': [], 'Resp': [], 'TNS': [], 'INS': [], 'TS': []}
+
+    total_files_processed = 0  # For percentage calculation
+    total_files = 3119
+    pbar = tqdm(total=total_files, desc="Processing files")
+   
+    for studyshort in folderqueue:
+        print("Study:", studyshort)
+        folder = config['NonQBox']['ASA24'][studyshort]
+        dag = config['Redcap']['datasources']['aabcarms'][studyshort]['dag']
+        sitenum = config['Redcap']['datasources']['aabcarms'][studyshort]['sitenum']
+    
+        if studyshort == 'UCLA':
+            # Handle UCLA's additional layer of folders
+            subfolders = folder_files(client, [folder])
+            for subfolder in subfolders:
+                subsubfolders = folder_files(client, [subfolder])
+                for subsubfolder in subsubfolders:
+                    process_folder(subsubfolder, big_data, studyshort)
+                    #total_files_processed += 1
+                    pbar.update(6)
+
+        else:
+            # Directly process folders for other study sites
+            subfolders = folder_files(client, [folder])
+            for subfolder in subfolders:
+                print(subfolder)
+                process_folder(subfolder, big_data, studyshort)
+                #total_files_processed += 1
+                pbar.update(6)
+        print(f"Completed processing for {studyshort}.")
+    pbar.close()
+
+    # Calculate and print running time
+    end_time = time.time()
+    running_time = end_time - start_time
+    print(f"Total running time: {running_time:.2f} seconds")
+
+
+
+    # Concatenate all DataFrames collected for each category
+    result_dfs = {category: pd.concat(dfs, ignore_index=True) if dfs else pd.DataFrame()
+                  for category, dfs in big_data.items()}
+
+    return result_dfs
+
+
+def process_folder(folder_id, big_data, studyshort):
+    subfilelist = box.list_of_files([folder_id])
+    #subfolders_id = folder_files(client, [folder_id])
+
+    for filename in subfilelist:
+
+        # Skip .zip files for UCLA
+        if studyshort == 'UCLA' and filename.endswith('.zip'):
+            print(f"Skipping .zip file: {filename}")
+
+            continue
+
+        # Download and process file based on its category
+        f = box.downloadFile(filename, download_dir="tmp", override_if_exists=True)
+        category = determine_category(f)
+        if category:
+            try:
+                # Assuming a function to process the file and return a DataFrame
+                df = process_file(f, category, filename)
+                pin = client.folder(folder_id=folder_id).get()
+                print(pin)
+                df['PIN'] = str(pin)[str(pin).find('HC'):str(pin).find('HC') + 13].strip(' ')
+                big_data[category].append(df)
+
+            except Exception as e:
+                print(f"Error processing file: {filename}, Error: {e}")
+
+def determine_category(filename):
+    # Determine file's category based on its name
+    if 'Totals' in filename:
+        return 'Totals'
+    elif 'Items' in filename:
+        return 'Items'
+    elif 'Resp' in filename:
+        return 'Resp'
+    elif 'TNS' in filename:
+        return 'TNS'
+    elif 'INS' in filename:
+        return 'INS'
+    elif 'TS' in filename:
+        return 'TS'
+    else:
+        return None
+
+def process_file(filepath, category, filename):
+    # Process the downloaded file and return a DataFrame
+    # This is a placeholder; actual processing will depend on file content and format
+    print(f"Processing {category} file: {filename}")
+
+    df = pd.read_csv(filepath)
+   
+    return df
+
+def mapping_list(df, prefixes):
+    """
+    Showing the mapping list
+
+    Parameters:
+    df (pd.DataFrame): The input DataFrame.
+    prefixes (list): A list of prefixes to identify the columns to be merged.
+
+    Returns:
+    mapping: The list with the merged columns.
+    """
+    # Extract the relevant column containing the variable names
+    variable_names = df['Variable / Field Name']
+
+    # Create a mapping from prefixed variables to their base variables
+    mapping = {}
+    for var in variable_names:
+        if any(var.startswith(prefix) for prefix in prefixes):
+            base_var = var.split('_', 1)[1]
+            mapping[var] = base_var
+    return mapping
+
+def merge_prefixed_columns(df, prefixes):
+    """
+    Merges columns in the DataFrame that start with any of the specified prefixes with their corresponding columns.
+
+    Parameters:
+    df (pd.DataFrame): The input DataFrame.
+    prefixes (list): A list of prefixes to identify the columns to be merged.
+
+    Returns:
+    pd.DataFrame: The DataFrame with the merged columns.
+    """
+    # Create a copy of the DataFrame to avoid modifying the original
+    df_merged = df.copy()
+
+    # Iterate over each prefix
+    for prefix in prefixes:
+        # Identify columns starting with the prefix
+        prefixed_columns = [col for col in df.columns if col.startswith(prefix)]
+
+        # Iterate over each prefixed column
+        for prefixed_col in prefixed_columns:
+            # Determine the corresponding column name by removing the prefix
+            base_col = prefixed_col[len(prefix):]
+
+            # Check if the corresponding column exists in the DataFrame
+            if base_col in df.columns:
+                # Merge the columns by taking the non-null values from either column
+                df_merged[base_col] = df_merged[prefixed_col].combine_first(df_merged[base_col])
+                # Drop the prefixed column after merging
+                df_merged.drop(columns=[prefixed_col], inplace=True)
+
+    return df_merged
+
+
+def clean_croms_income(value):
+    # Remove punctuation (commas, periods)
+    value = re.sub(r'[,.]', '', str(value)).strip()
+
+    # Handle empty values and -9999 as missing data
+    if value == '' or value == '-9999' or value == '-999':
+        return np.nan
+
+    # If it's a range, calculate the average
+    if '-' in value:
+        try:
+            low, high = map(int, value.split('-'))
+            return (low + high) / 2
+        except ValueError:
+            return np.nan  # In case the range contains invalid values
+    else:
+        try:
+            return int(value)
+        except ValueError:
+            return np.nan  # In case it's not a valid number
+
+
+#def swapmeds(oldDF=HCPDparentmeds, medmap=MAP, medvarlist=medlist):
+def swapmeds(oldDF, medmap, medvarlist):
     ### Function maps a list of medication typos (or some other map dictionary) to a clean df of medication names or a yes/no class (or whatever is being replaced).
     DP=oldDF.copy()
     for m in medvarlist:
