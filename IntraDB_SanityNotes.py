2d1d236889698b3df33be79f17ba5f000780e07c
petralenzini <petra.lenzini@gmail.com>
2025-06-27 15:42:11 -0500

    update sanity filters

--- IntraDB_SanityNotes.py
+++ IntraDB_SanityNotes.py
@@ -3,10 +3,13 @@ from functions import *
 from config import *
 import os
 from datetime import date
+import subprocess
 import json
+
 import datetime
 
 ## get configuration files
+
 outp="/Users/petralenzini/work/Behavioral/AABC/AABC_Behavioral_QC/AABC_Behavioral_QC/tmp/"
 config = LoadSettings("/Users/petralenzini/work/Behavioral/AABC/AABC_Behavioral_QC/AABC_Behavioral_QC/config.yml")
 secret=pd.read_csv(config['config_files']['secrets'])
@@ -15,16 +18,18 @@ user = intradb.user[0]
 passw = intradb.auth[0]
 HOST = "hcpi-shadow22.nrg.wustl.edu"
 projects=["AABC_MGH_ITK","AABC_UMN_ITK","AABC_UCLA_ITK","AABC_WU_ITK"]
+freeze2list=pd.read_csv('/Users/petralenzini/work/Behavioral/AABC/AABC_Behavioral_QC/AABC_Behavioral_QC/Union_HCA-AABC_Data-Freeze-2_id-events_2025-04-25.csv')
 #test
 #curlcmd0 = "curl -s -k -v -u "+user+":"+passw+" https://intradb.humanconnectome.org/data/experiments?xsiType=xnat:mrSessionData\&format=json\&columns=ID,label,project,xsiType,subject_label,URI\&project=AABC_MGH_ITK"
 #curlcmd0 = "curl -s -k -v -u "+user+":"+passw+" https://intradb.humanconnectome.org/data/experiments?xsiType=xnat:subjectData\&format=json\&columns=subject_label,yob\&project=AABC_MGH_ITK"
 
-PROJECT='AABC_WU_ITK'
+#PROJECT='AABC_MGH_ITK'
 AllSanity=pd.DataFrame()
 for PROJECT in projects:
     curlcmd="curl -s -k -v -u "+user+":"+passw+" https://"+HOST+"/xapi/sanityChecksReports/project/"+PROJECT+"/failureReportCSV > "+outp+PROJECT+"_Sanity.csv"
     os.system(curlcmd)
     sanitydf=pd.read_csv(outp+PROJECT+"_Sanity.csv")
+    sanitydf=sanitydf.loc[~(sanitydf.scanID.isin(["10-MR1","10-MR2"]))]
     sanitydf['scanID'] = sanitydf['scanID'].fillna(-1)
     sanitydf=sanitydf.loc[sanitydf.scanner.isnull()==False].copy()
     sanitydf['scanID'] = sanitydf['scanID'].astype(int)
@@ -82,7 +87,6 @@ AllSanity=AllSanity.drop(columns=['imageSessionID','label','site','ID'])
 AllSanity=AllSanity.rename(columns={'':'MR_Session'})
 AllSanity.to_csv(outp+'SanityChecksWithNotes_'+date.today().strftime("%d%b%Y")+'.csv',index=False)
 
-#####
 
 inventory=pd.read_csv('Union-Freeze_AABC-HCA_VIV_2024-05-13.csv')
 inventory=inventory[['Site','PIN']].copy()
@@ -102,3 +106,48 @@ MRSmerge=m7.rename(columns={'Date':'7T date','MR ID':'7T MR ID'}).merge(d3A.rena
 MRSmerge=MRSmerge.merge(d3B.rename(columns={'Date':'3T Session B Date','MR ID':'3T Session B MR ID'}),how='left',on='PIN')
 #MRSmerge['3A to 7T days passed']=(date(MRSmerge['7T date'])-date(MRSmerge['3T Session A Date']))
 MRSmerge.drop(columns=['PIN'])[['3T Session A MR ID','3T Session A Date','3T Session B MR ID','3T Session B Date','7T MR ID','7T date']].to_csv('MRSdates.csv',index=False)
+
+
+#compare status of INTAKE to STG directory for Freeze2
+def getsessionlist(proj = "AABC_STG"):
+    # Build the curl command
+    curlcmd = [
+        "curl", "-s", "-k", "-u", f"{user}:{passw}",
+        f"https://intradb.humanconnectome.org/data/experiments?xsiType=xnat:mrSessionData&format=json&columns=ID,label,project,xsiType,subject_label,URI&project={proj}"
+    ]
+    # Run curl and capture the output
+    try:
+        result = subprocess.check_output(curlcmd, text=True)
+        data = json.loads(result)
+        # Extract label values
+        labels = [item['label'] for item in data['ResultSet']['Result']]
+        return labels
+
+    except subprocess.CalledProcessError as e:
+        print("Curl command failed:", e)
+    except json.JSONDecodeError as e:
+        print("Failed to parse JSON:", e)
+    except KeyError as e:
+        print("Unexpected data format, missing key:", e)
+
+#get STG list
+S=getsessionlist(proj = "AABC_STG")
+H=getsessionlist(proj = "CCF_HCA_STG")
+
+STG=pd.DataFrame(S+H)
+STG[['subject', 'redcap_event','other']] = STG[0].str.split('_', n=3, expand=True)
+STG['STG']='YES'
+
+#Get itk lists
+MGH=getsessionlist(proj = "AABC_MGH_ITK")
+UMN=getsessionlist(proj = "AABC_UMN_ITK")
+WU=getsessionlist(proj = "AABC_WU_ITK")
+UCLA=getsessionlist(proj = "AABC_UCLA_ITK")
+
+ITK=pd.DataFrame(MGH+UMN+WU+UCLA)
+ITK[['subject', 'redcap_event','other']] = ITK[0].str.split('_', n=3, expand=True)
+ITK['AABC_ITK']='YES'
+#merge STG and ITK lists with freeze2 list
+FZa = pd.merge(freeze2list[['study', 'subject', 'redcap_event', 'M/F', 'Site', 'event_date', 'Freeze']],ITK[['subject','redcap_event','AABC_ITK']].drop_duplicates(),on=['redcap_event','subject'],how='left')
+FZb = pd.merge(FZa,STG[['subject','redcap_event','STG']].drop_duplicates(),on=['redcap_event','subject'],how='left')
+FZb.to_csv('Union_HCA-AABC_Data-Freeze-2_ImgStatus_2025-04-25.csv',index=False)
