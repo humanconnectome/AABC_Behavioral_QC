a485f96db7fa0db12fa7250a593f91dff40f7c63
petralenzini <petra.lenzini@gmail.com>
2025-06-27 15:38:52 -0500

    restore Petra

--- tlbxscan.py
+++ tlbxscan.py
@@ -29,10 +29,20 @@ scratch=205351313707
 # # 6. create and send snapshot of patched data to BOX after dropping restricted variables
 
 ##FIRST THE RAW DATA FILES
-tlbxraw4=importTLBX(siteabbrev='WU',typed='raw')
-tlbxraw1=importTLBX(siteabbrev='MGH',typed='raw')
-tlbxraw3=importTLBX(siteabbrev='UMN',typed='raw')
-tlbxraw2=importTLBX(siteabbrev='UCLA',typed='raw')
+tlbxdate="2025-06-17"
+
+#navigate to plenzini@login3.chpc.wustl.edu:/home/tools/catTLBX and run
+#./getTLBX_last.sh AABC_UMN_ITK "Assessment Data" DataUMN.csv#
+#for all the ITK folders and string types
+
+#tlbxraw4=importTLBX(siteabbrev='WU',typed='raw')
+#tlbxraw1=importTLBX(siteabbrev='MGH',typed='raw')
+#tlbxraw3=importTLBX(siteabbrev='UMN',typed='raw')
+#tlbxraw2=importTLBX(siteabbrev='UCLA',typed='raw')
+tlbxraw4=pd.read_csv(outp+"DataWU_"+tlbxdate+".csv",low_memory=False)
+tlbxraw1=pd.read_csv(outp+"DataMGH_"+tlbxdate+".csv",low_memory=False)
+tlbxraw3=pd.read_csv(outp+"DataUMN_"+tlbxdate+".csv",low_memory=False)
+tlbxraw2=pd.read_csv(outp+"DataUCLA_"+tlbxdate+".csv",low_memory=False)
 
 #send this to temp
 rf2=pd.concat([tlbxraw1,tlbxraw2,tlbxraw3,tlbxraw4])
@@ -41,15 +51,34 @@ rf2.to_csv(outp+"temp_TLBX_RAW.csv",index=False)
 
 
 #NOW THE SCORED DATA
-tlbxscore4=importTLBX(siteabbrev='WU',typed='scores')
-tlbxscore1=importTLBX(siteabbrev='MGH',typed='scores')
-tlbxscore3=importTLBX(siteabbrev='UMN',typed='scores')
-tlbxscore2=importTLBX(siteabbrev='UCLA',typed='scores')
-tlbxscore1['site']=1
-tlbxscore2['site']=2
-tlbxscore3['site']=3
-tlbxscore4['site']=4
+#tlbxscore4=importTLBX(siteabbrev='WU',typed='scores')
+#tlbxscore1=importTLBX(siteabbrev='MGH',typed='scores')
+#tlbxscore3=importTLBX(siteabbrev='UMN',typed='scores')
+#tlbxscore2=importTLBX(siteabbrev='UCLA',typed='scores')
+tlbxscore4=pd.read_csv(outp+"ScoresWU_"+tlbxdate+".csv",low_memory=False)
+tlbxscore1=pd.read_csv(outp+"ScoresMGH_"+tlbxdate+".csv",low_memory=False)
+tlbxscore3=pd.read_csv(outp+"ScoresUMN_"+tlbxdate+".csv",low_memory=False)
+tlbxscore2=pd.read_csv(outp+"ScoresUCLA_"+tlbxdate+".csv",low_memory=False)
+
 
 
 dffull=pd.concat([tlbxscore1, tlbxscore3, tlbxscore2, tlbxscore4])
 dffull.to_csv(outp+"temp_TLBX_Scores.csv",index=False)
+
+#NOW THE Registration DATA
+#tlbxscore4=importTLBX(siteabbrev='WU',typed='scores')
+#tlbxscore1=importTLBX(siteabbrev='MGH',typed='scores')
+#tlbxscore3=importTLBX(siteabbrev='UMN',typed='scores')
+#tlbxscore2=importTLBX(siteabbrev='UCLA',typed='scores')
+tlbxReg4=pd.read_csv(outp+"RegistWU_"+tlbxdate+".csv",low_memory=False)
+tlbxReg1=pd.read_csv(outp+"RegistMGH_"+tlbxdate+".csv",low_memory=False)
+tlbxReg3=pd.read_csv(outp+"RegistUMN_"+tlbxdate+".csv",low_memory=False)
+tlbxReg2=pd.read_csv(outp+"RegistUCLA_"+tlbxdate+".csv",low_memory=False)
+
+tlbxReg1['site']=1
+tlbxReg2['site']=2
+tlbxReg3['site']=3
+tlbxReg4['site']=4
+
+dfreg=pd.concat([tlbxReg1, tlbxReg3, tlbxReg2, tlbxReg4])
+dfreg.to_csv(outp+"temp_TLBX_Registration.csv",index=False)
