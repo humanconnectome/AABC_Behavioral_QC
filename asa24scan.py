eda12268a0bafda155901dbd21da4812f7269289
petralenzini <petra.lenzini@gmail.com>
2025-06-27 15:57:03 -0500

    comments out unnecessary tasks

--- asa24scan.py
+++ asa24scan.py
@@ -20,15 +20,17 @@ secret=pd.read_csv(config['config_files']['secrets'])
 box = LifespanBox(cache=outp)
 #scratch=205351313707
 
-folderqueue=['MGH','WU','UMN','UCLA']
+folderqueue=['WU','UMN','UCLA','MGH']#,
 client = box.get_client()
-ALLSUBS,BIGGESTTotals,BIGGESTItems,BIGGESTResp,BIGGESTTS,BIGGESTTNS,BIGGESTINS=getASA(client=client,folderqueue=folderqueue)
+#ALLSUBS,BIGGESTTotals,BIGGESTItems,BIGGESTResp,BIGGESTTS,BIGGESTTNS,BIGGESTINS=getASA(client=client,folderqueue=folderqueue)
+ALLSUBS,BIGGESTTotals=getASA(client=client,folderqueue=folderqueue)
+
 ALLSUBS.to_csv(outp+'temp_ALLSUBS.csv',index=False)
 BIGGESTTotals.to_csv(outp+'temp_Totals.csv',index=False)
-BIGGESTItems.to_csv(outp+'temp_Items.csv',index=False)
-BIGGESTResp.to_csv(outp+'temp_Resp.csv',index=False)
-BIGGESTTS.to_csv(outp+'temp_TTS.csv',index=False)
-BIGGESTTNS.to_csv(outp+'temp_TNS.csv',index=False)
-BIGGESTINS.to_csv(outp+'temp_INS.csv',index=False)
+#BIGGESTItems.to_csv(outp+'temp_Items.csv',index=False)
+#BIGGESTResp.to_csv(outp+'temp_Resp.csv',index=False)
+#BIGGESTTS.to_csv(outp+'temp_TTS.csv',index=False)
+#BIGGESTTNS.to_csv(outp+'temp_TNS.csv',index=False)
+#BIGGESTINS.to_csv(outp+'temp_INS.csv',index=False)
 
 print("THE END")
